cb5c41972f5295359f344fa404afec9b17b90f7b
nitinkumar.pawar <nitinkumar.pawar@arrkgroup.com>
2023-01-19 13:52:41 +0530

    assignment_2: Updated 'def post_question()' Updated 'def getAnswer_by_questionId()'

--- venv/app.py
+++ venv/app.py
@@ -4,17 +4,16 @@ import json
 from pprint import pprint
 
 dynamo_client = boto3.resource(service_name='dynamodb', region_name='eu-west-1',
-                               aws_access_key_id='ASIAX2KHRAJA4VO2RUPM',
-                               aws_secret_access_key='HGH7hr2HxkaKue30Tm6WFybQ+Q2iRCm4RXwKkjAd',
-                               aws_session_token='IQoJb3JpZ2luX2VjEO3//////////wEaCWV1LXdlc3QtMSJIMEYCIQCCPTVdBzFfDZe37hymnxFOA5sobXkrK6sjOw25XCIK0gIhAK8RmvfTmb9rCqa32iKKJV1TdNK9zVF6Cnm3BnzWG8oWKpsDCFYQAxoMNTM3NTU3Nzk1MzkzIgxi3vL21dcONBO+s9Eq+AJ2uEmeXpkw0lIXiZ81bOnY2IwsH0mMNsN/oLuNqm9F3dOApCGbVYA/WCf976KIavT0LZPTF+NGWaupnmDxny8WH8e1UzcMNIj/Ral06boCt8tybhTBbQki67xmvMFoXHA0KzYhLi8BpYbs1BWpl2BXDbZkLrt5eYF9QEurdymqY9BAfz962hpDZPRdgEYvPosZVya6tr/3xbUKuEMe3fFawHmNU8vcpIwibg4hkP5sEKi3w/aXGSzB94uW5l6Xy+vic9I3esOzOBEbOkBePSYE+Sks+sOLEOJ3x67OKM9pt+XgLz60QZY+zLBaCwQ0/hAFm+1DRz7KRFDSoJdFf5xudxoMa7Vqkvjf7+A4+Fy7v6oQRXxrfBYOs0OEKxit24W1ro6gRf4xoHoU1bt60EPYz5EzrcIjq83wbch/XVOyq5eEj+RYTjdfACjRUogxxb1W6eMFgmjLjSaL/d68aSs+YLrnSuD6ZvHdmEFGnTEOheGxkn7DwQN9MJv1nZ4GOqUBEawkdZFLUp+OIb28vI1Q+6M7F0PhEPkqQCd3Zj7WP6RRuji2F8Ee+RkSHS49FXG6QOoT2z56BT58RfVXVhA3DyhLNv+8Xq5fvyZB4Vc33lDP7VIEHRxXd1PJ5ItA2gys6yP/kH3E9JmOdR12o42JsL7IlKWLhcIkYEhFm4nHorZhlE2/wqydjf94plkdOeLkTpeBUV1E3jbzss/w+xhq7MkWNvf5')
+                               aws_access_key_id='ASIAX2KHRAJAU5KPS4XH',
+                               aws_secret_access_key='V+OXAZAGto/STf/01jNZFmkqub+f+p/3pXcBhljY',
+                               aws_session_token='IQoJb3JpZ2luX2VjEAYaCWV1LXdlc3QtMSJHMEUCIQDYMX1uBq9pKjmRid8PRkuIlEflCv8tOdr/THaaQtvvUQIgavoQTgVjswle45Px7+fALSgFeqpv/bXLFGYqJ3D9JmoqmwMIbxADGgw1Mzc1NTc3OTUzOTMiDJJrIJj0qiaDSesKqir4AqCqdNV1BQD0DcmUHuPKVhhU5Xpmh+xRb5bwO9s82bxzm1z3Zi8CPkG4CUnVfUVitJOLJawlPCQLBWudPPMMhFscS7z6wSot9iPbD8LpaNYOZekGTintOeMf+gxL/j/k0pNaEqOF/CdjiG4zA7FSO9HdxXg8xyKvNJjoam8P/GOCaZht2ozdCb88uT6Tn8YLPjSqkOsJn9ytWkidNsqqddXY3ZwtuYuJ72+UBTS5xTzpBbOh1hyHpCxM6yuvRAInlGPaJPoEOtRPkhG1MFniu4e2d0Rt6qKKQU86MsC5SrMnl2bJJQCpDnwEPW89HyRbiVVOAGr6j308jUFoVoEn2BJ53pvnX2rLInqVdzVe/HM03BcPkYr9OIcH8yNZdocnLjT80Nz9z9/Tvjr7EYWOpYiKnrxHQAaYIOX5IoItuYqFWjxU5WoCDQ9rC9ru8Uz15xxExn8HbZxXwvS4IAzpdGaKfaMKEyhGRt/xILH9b4RrdC5At874lU4wrqqjngY6pgHThuLGLtPPoImVBbXZlXAomnE9nVEcyAPxbchR1SlZOQNmyneg69VnUtTw8jE/yCe8cEjlUzoWE/6KXKSiu0yaoVAH9RfBLkMnGdUhtbWBFGwAlXrg9V/7Q/lZwjDlUssa+z+PrZq5C9ec+2V0My7IiF/rK+vJY28aWknt+Korfo2xoVHEGvD7IV/6/knVucjZzl9rY4cUCez63cRHmyFlylIC93zs')
 __connected_table__ = dynamo_client.Table("freshers-example")
 
 print(__connected_table__.table_status)
 print("\n")
 
 def post_questions():
-    type = input("Please the 'type' (i.e. Partition Key): ")
-    # sortKey = input("Please enter the sortKey to be stored: ")
+    type = "question"
     question = input("Enter the question: ")
     questionId = input("Enter the questionId: ")
     userId = input("Enter the userId as your emailId: ")
@@ -31,22 +30,19 @@ def post_questions():
         Item=item_details
     )
     print("Data Entered Successfully")
-    # print(data['Items'])
 post_questions()
 
 def getAnswer_by_questionId():
-    sortKey = input("Please enter the sortKey to get all answers by questionId: ")
-    data = __connected_table__.scan(
-        FilterExpression=Key('sortKey').eq(sortKey)
+    type = "answer"
+    print("Please enter following details to get the answer by questionId: ")
+    questionId = input("questionId: ")
+    userId = input("userId: ")
+    sortKey = str(type + "#" + questionId + "#" + userId)
+    data = __connected_table__.get_item(
+        Key={'type': type, 'sortKey': sortKey},
+        TableName='freshers-example'
     )
-    #print(data['Items'])
-    print("Entered 'sortKey': "+sortKey)
-    print("[")
-    for i in data['Items']:
-        print("   { ")
-        print("    " + i['answer'])
-        print("   }")
-    print("]")
+    print(data)
 getAnswer_by_questionId()
 
 def getAll_question_by_userId():
