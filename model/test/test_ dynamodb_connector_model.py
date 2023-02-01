a486aa6c834402c47a2fae5da169bbd0b579542d
nitinkumar_p <nitinkumar.pawar@arrkgroup.com>
2023-02-02 01:04:36 +0530

    update

--- model/test/test_ dynamodb_connector_model.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-from model.dynamodb_connector_model import connection_test
-
-
-def test_dynamodb_connector_model():
-    assert connection_test() == "ACTIVE"
--- model/test/test_question_model.py
+++ model/test/test_question_model.py
@@ -1,5 +1,26 @@
+from unittest.mock import MagicMock
 
-
+from model import dynamodb_connector_model
 from model.question_model import question_model
-obj = question_model()
 
+obj = dynamodb_connector_model
+
+
+def test_post_questions_model(mocker):
+    mock_put_item = MagicMock(return_value=None)
+    mocker.patch.object(dynamodb_connector_model.__connected_table__, 'put_item', mock_put_item)
+    data = {
+        'question': 'test',
+        'userId': 'nk@gmail.com'
+    }
+    question_model().post_questions_model(data)
+    mock_put_item.assert_called_with(
+        TableName='freshers_example',
+        Item={
+            'type': 'question',
+            'sortKet': 'question#nk@gmail.com#id_db.hex',
+            'question': 'test',
+            'status': '1',
+            'userId': 'nk@gmail.com'
+        }
+    )
